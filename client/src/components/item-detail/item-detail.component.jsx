4d7674320b7706410b599ab5c23b8452984d7e63
Andrea Calin <andrea.n.calin@gmail.com>
2020-06-08 14:16:55 +0200

    mobile compatiblity added

--- client/src/components/item-detail/item-detail.component.jsx
+++ client/src/components/item-detail/item-detail.component.jsx
@@ -14,67 +14,58 @@ class ItemDetail extends React.Component {
         const images = [];
 
         return (
-            <div>
-                <div className="row">
-                    <h1>
-                        {item.name}
-                    </h1>
-                </div>
-                <div className="row">
-                    <div>
-                        {item.additionalImages &&
-                            <div className='slideshow-image'>
-                                <Slideshow images={item.additionalImages ? images.concat(item.imageUrl).concat(item.additionalImages) : images.concat(item.imageUrl)}></Slideshow>
-                            </div>
-                        }
-                        {!item.additionalImages &&
-                            <div>
-                                <img className='background-detail-image' src={item.imageUrl}></img>
-                            </div>
-                        }
-                    </div>
-                </div>
-                <div className="row">
-                    <div>
-                        <DescriptionContainer>{item.description}</DescriptionContainer>
-                    </div>
-                </div>
-                {/* <div className="row">
-                    <div>
-                        <span>Preț: {item.price} ron</span>
-                    </div>
-                </div> */}
-                <div className="row">
-                    <div>
-                        <AddButton onClick={() => this.props.addItem(item)} inverted>
-                            Adaugă în coș
-                        </AddButton>
-                    </div>
-                </div>
-            </div >
-            /* <CollectionItemContainer>
+            // <div>
+            //     <div className="row">
+            //         <h1>
+            //             {item.name}
+            //         </h1>
+            //     </div>
+            //     <div className="row">
+            //         <div>
+            //             {item.additionalImages &&
+            //                 <div className='slideshow-image'>
+            //                     <Slideshow images={item.additionalImages ? images.concat(item.imageUrl).concat(item.additionalImages) : images.concat(item.imageUrl)}></Slideshow>
+            //                 </div>
+            //             }
+            //             {!item.additionalImages &&
+            //                 <div>
+            //                     <img className='background-detail-image' src={item.imageUrl}></img>
+            //                 </div>
+            //             }
+            //         </div>
+            //     </div>
+            //     <div className="row">
+            //         <div>
+            //             <DescriptionContainer>{item.description}</DescriptionContainer>
+            //         </div>
+            //     </div>
+            //     {/* <div className="row">
+            //         <div>
+            //             <span>Preț: {item.price} ron</span>
+            //         </div>
+            //     </div> */}
+            //     <div className="row">
+            //         <div>
+            //             <AddButton onClick={() => this.props.addItem(item)} inverted>
+            //                 Adaugă în coș
+            //             </AddButton>
+            //         </div>
+            //     </div>
+            // </div >
+            <CollectionItemContainer>
+                <BackgroundImage className='image' images={item.additionalImages ? images.concat(item.imageUrl).concat(item.additionalImages) : images.concat(item.imageUrl)} />
                 <CollectionFooterContainer>
                     <NameContainer>{item.name}</NameContainer>
-                    <Slideshow></Slideshow>
-                    <DescriptionContainer>{item.description}</DescriptionContainer>
-                    <AddButton inverted>
-                        Adaugă în coș
-                    </AddButton>
+                    <PriceContainer>{item.price} ron</PriceContainer>
                 </CollectionFooterContainer>
-            </CollectionItemContainer> */
-            /* <CollectionItemContainer>
-                        <CollectionFooterContainer>
-                            
-                            <PriceContainer>{item.price}</PriceContainer>
-                            <DescriptionContainer>{item.description}</DescriptionContainer>
-                        </CollectionFooterContainer>
-                        <AddButton inverted>
-                            Adaugă în coș
-                    </AddButton>
-                    </CollectionItemContainer> */
-            /* </div> */
+                <AddButton onClick={() => addItem(item)} inverted>
+                    Adaugă în coș
+                </AddButton>
+                <DescriptionContainer>{item.description}</DescriptionContainer>
 
-        )
+            </CollectionItemContainer>
+        );
+
     }
 
 }
--- client/src/components/item-detail/item-detail.styles.jsx
+++ client/src/components/item-detail/item-detail.styles.jsx
@@ -1,12 +1,83 @@
+// import styled from 'styled-components';
+// import CustomButton from '../custom-button/custom-button.component';
+
+// export const CollectionItemContainer = styled.div`
+//   width: 80%;
+//   display: flex;
+//   flex-direction: row;
+//   height: 50%;
+//   position: relative;
+//   &:hover {
+//     .image {
+//       opacity: 0.8;
+//     }
+//     button {
+//       opacity: 0.85;
+//       display: flex;
+//     }
+//   }
+//   @media screen and (max-width: 800px) {
+//     width: 40vw;
+//     &:hover {
+//       .image {
+//         opacity: unset;
+//       }
+//       button {
+//         opacity: unset;
+//       }
+//     }
+//   }
+// `;
+
+// export const CollectionFooterContainer = styled.div`
+//   width: 100%;
+//   height: 20%;
+//   display: flex;
+//   justify-content: space-between;
+//   font-size: 18px;
+// `;
+
+// export const NameContainer = styled.span`
+// height: 10%;
+// weight: 10%;
+//   margin-bottom: 15px;
+// `;
+
+// export const PriceContainer = styled.span`
+// height: 10%;
+// weight: 50%;
+// text-align: right;
+// float: left;
+// display: flex;
+// `;
+
+// export const DescriptionContainer = styled.span`
+//   weight: 50%;
+//   height: 50%;
+//   float: left;
+// `;
+
+// export const AddButton = styled(CustomButton)`
+//   float: left;
+//   width: 10%;
+//   opacity: 0.7;
+//   @media screen and (max-width: 800px) {
+//     width: 40%;
+//     display: block;
+//     opacity: 0.9;
+//     min-width: unset;
+//     padding: 0 10px;
+//   }
+// `;
+
 import styled from 'styled-components';
 import CustomButton from '../custom-button/custom-button.component';
+import Slideshow from '../image-slide-show/image.slide.show.component'
 
 export const CollectionItemContainer = styled.div`
-  width: 80%;
-  display: flex;
-  flex-direction: row;
-  height: 50%;
-  position: relative;
+  width: 40%;
+  height: 400px;
+  align-items: center;
   &:hover {
     .image {
       opacity: 0.8;
@@ -29,43 +100,48 @@ export const CollectionItemContainer = styled.div`
   }
 `;
 
+export const AddButton = styled(CustomButton)`
+width: 40%;
+opacity: 0.7;
+position: absolute;
+top: 535px;
+display: none;
+@media screen and (max-width: 800px) {
+  display: block;
+  opacity: 0.9;
+  min-width: unset;
+  padding: 0 10px;
+}
+`;
+
+export const BackgroundImage = styled(Slideshow)`
+  width: 40%;
+  height: 95%;
+  margin-bottom: 5px;
+`;
+
 export const CollectionFooterContainer = styled.div`
   width: 100%;
-  height: 20%;
+  height: 5%;
   display: flex;
   justify-content: space-between;
   font-size: 18px;
 `;
 
 export const NameContainer = styled.span`
-height: 10%;
-weight: 10%;
+  width: 80%;
   margin-bottom: 15px;
 `;
 
+export const DescriptionContainer = styled.div`
+  width: 70%;
+  position: absolute;
+  top: 535px;
+  margin-top: 50px;
+  font-size: 18px;
+`;
+
 export const PriceContainer = styled.span`
-height: 10%;
-weight: 50%;
-text-align: right;
-float: left;
-display: flex;
-`;
-
-export const DescriptionContainer = styled.span`
-  weight: 50%;
-  height: 50%;
-  float: left;
-`;
-
-export const AddButton = styled(CustomButton)`
-  float: left;
-  width: 10%;
-  opacity: 0.7;
-  @media screen and (max-width: 800px) {
-    width: 40%;
-    display: block;
-    opacity: 0.9;
-    min-width: unset;
-    padding: 0 10px;
-  }
+  width: 20%;
+  text-align: right;
 `;
