dc424745b7cf4b74089296a71f1bae2fc2470df7
Andrea Calin <andrea.n.calin@gmail.com>
2020-06-11 16:21:04 +0200

    deliveryAddress form added to checkout

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -28,88 +28,161 @@ import {
 
 import './accordion.styles.scss';
 
+import CustomButton from '../../components/custom-button/custom-button.component';
+import FormInput from '../../components/form-input/form-input.component';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div>
+const CheckoutPage = ({ cartItems, total }) => {
 
-    <CheckoutPageContainer>
-      <CheckoutHeaderContainer>
-        <HeaderBlockContainer>
-          <span>Produs</span>
-        </HeaderBlockContainer>
-        <HeaderBlockContainer>
-          <span>Descriere</span>
-        </HeaderBlockContainer>
-        <HeaderBlockContainer>
-          <span>Cantitate</span>
-        </HeaderBlockContainer>
-        <HeaderBlockContainer>
-          <span>Preț</span>
-        </HeaderBlockContainer>
-        <HeaderBlockContainer>
-          <span>Șterge</span>
-        </HeaderBlockContainer>
-      </CheckoutHeaderContainer>
-      {cartItems.map(cartItem => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-      <TotalContainer>Preț total: {total} RON</TotalContainer>
-      <WarningContainer>
-        *Folosește următorul card de credit pentru testare*
+  const [orderProperties, setOrderProperties] = useState({ displayName: '', phoneNumber: '', region: '', city: '', completeAddress: '', otherInstructions: '' });
+  const { displayName, phoneNumber, region, city, completeAddress, otherInstructions } = orderProperties;
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+
+    // signUpStart({ displayName, email, password })
+
+  };
+
+  const handleChange = event => {
+    const { value, name } = event.target;
+
+    setOrderProperties({ ...orderProperties, [name]: value });
+  };
+
+  return (
+    <div>
+
+      <CheckoutPageContainer>
+        <CheckoutHeaderContainer>
+          <HeaderBlockContainer>
+            <span>Produs</span>
+          </HeaderBlockContainer>
+          <HeaderBlockContainer>
+            <span>Descriere</span>
+          </HeaderBlockContainer>
+          <HeaderBlockContainer>
+            <span>Cantitate</span>
+          </HeaderBlockContainer>
+          <HeaderBlockContainer>
+            <span>Preț</span>
+          </HeaderBlockContainer>
+          <HeaderBlockContainer>
+            <span>Șterge</span>
+          </HeaderBlockContainer>
+        </CheckoutHeaderContainer>
+        {cartItems.map(cartItem => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+        <TotalContainer>Preț total: {total} RON</TotalContainer>
+        <WarningContainer>
+          *Folosește următorul card de credit pentru testare*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </WarningContainer>
-      <StripeCheckoutButton price={total} />
-    </CheckoutPageContainer>
+        <StripeCheckoutButton price={total} />
+      </CheckoutPageContainer>
 
-    <Accordion allowMultipleExpanded={true} allowZeroExpanded={true}>
-      <AccordionItem>
-        <AccordionItemHeading>
-          <AccordionItemButton>
-            Adresa de livrare
+      <form className='checkout-form' onSubmit={handleSubmit}>
+        <Accordion allowMultipleExpanded={true} allowZeroExpanded={true}>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Adresa de livrare
+          </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <FormInput
+                type='text'
+                name='displayName'
+                value={displayName}
+                onChange={handleChange}
+                label='Persoană de contact'
+                required
+              />
+              <FormInput
+                type='text'
+                name='phoneNumber'
+                value={phoneNumber}
+                onChange={handleChange}
+                label='Număr de telefon pentru contact'
+                required
+              />
+              <FormInput
+                type='text'
+                name='country'
+                value={'România'}
+                onChange={handleChange}
+                label='Țara'
+                disabled
+              />
+              <FormInput
+                type='text'
+                name='region'
+                value={region}
+                onChange={handleChange}
+                label='Județ'
+                required
+              />
+              <FormInput
+                type='text'
+                name='city'
+                value={city}
+                onChange={handleChange}
+                label='Oraș'
+                required
+              />
+              <FormInput
+                type='text'
+                name='completeAddress'
+                value={completeAddress}
+                onChange={handleChange}
+                label='Strada, Număr (*Bloc, Scară, Apartament, Etaj)'
+                required
+              />
+              <FormInput
+                type='text'
+                name='otherInstructions'
+                value={otherInstructions}
+                onChange={handleChange}
+                label='Alte instrucțiuni (opțional)'
+              />
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Metoda de livrare
                     </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel>
-          <p>
-            Exercitation in fugiat est ut ad ea cupidatat ut in
-            cupidatat occaecat ut occaecat consequat est minim minim
-            esse tempor laborum consequat esse adipisicing eu
-            reprehenderit enim.
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur
+                ea in ut nostrud velit in irure cillum tempor laboris
+                sed adipisicing eu esse duis nulla non.
                     </p>
-        </AccordionItemPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <AccordionItemHeading>
-          <AccordionItemButton>
-            Metoda de livrare
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Metoda de plata
                     </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel>
-          <p>
-            In ad velit in ex nostrud dolore cupidatat consectetur
-            ea in ut nostrud velit in irure cillum tempor laboris
-            sed adipisicing eu esse duis nulla non.
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur
+                ea in ut nostrud velit in irure cillum tempor laboris
+                sed adipisicing eu esse duis nulla non.
                     </p>
-        </AccordionItemPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <AccordionItemHeading>
-          <AccordionItemButton>
-            Metoda de plata
-                    </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel>
-          <p>
-            In ad velit in ex nostrud dolore cupidatat consectetur
-            ea in ut nostrud velit in irure cillum tempor laboris
-            sed adipisicing eu esse duis nulla non.
-                    </p>
-        </AccordionItemPanel>
-      </AccordionItem>
-    </Accordion>
-  </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
+      </form>
 
-);
+    </div>
+  )
+}
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
