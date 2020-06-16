cc7987db157078246e9fa9e7b100a6303ec450d0
Andrea Calin <andrea.n.calin@gmail.com>
2020-06-16 11:42:16 +0200

    push notifications for checkout page

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store, persistor } from './redux/store';
-import * as serviceWorker from './serviceWorker';
+import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { ToastProvider } from "react-toast-notifications";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <ToastProvider placement='top-center' autoDismiss={true} autoDismissTimeout='5000'>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ToastProvider>,
+  document.getElementById("root")
 );
 
 serviceWorker.register();
--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {
@@ -32,16 +31,18 @@ import './accordion.styles.scss';
 
 import { sendOrderStart } from '../../redux/shop/shop.actions'
 
-import CustomButton from '../../components/custom-button/custom-button.component';
 import FormInput from '../../components/form-input/form-input.component';
 import { CheckoutItemContainer } from '../../components/checkout-item/checkout-item.styles'
+
+import { useToasts } from 'react-toast-notifications'
+
 
 const CheckoutPage = ({ sendOrderStart, cartItems, total, orderDetails }) => {
 
   const [orderProperties, setOrderProperties] = useState({ displayName: '', phoneNumber: '', region: '', city: '', completeAddress: '', otherInstructions: '' });
   const { displayName, phoneNumber, region, city, completeAddress, otherInstructions } = orderProperties;
 
-  const [paymentMethod, setPaymentMethod] = useState('bankTransfer');
+  const [paymentMethod, setPaymentMethod] = useState();
   const [totalPriceWithTransport, setTotalPriceWithTransport] = useState(total);
 
   const handleRadioChange = event => {
@@ -53,10 +54,20 @@ const CheckoutPage = ({ sendOrderStart, cartItems, total, orderDetails }) => {
     }
   }
 
+  const { addToast } = useToasts()
+
 
   const handleSubmit = event => {
     event.preventDefault();
 
+    if (!displayName || !phoneNumber || !region || !city || !completeAddress || !completeAddress.length > 8) {
+      addToast("Adresa este incompletă! Vă rugăm completați toate câmpurile.", { appearance: 'error' })
+      return;
+    }
+    if (!paymentMethod) {
+      addToast("Vă rugăm selectați metoda de livrare și plată!", { appearance: 'error' })
+      return;
+    }
     sendOrderStart(orderProperties);
 
   }
@@ -69,7 +80,6 @@ const CheckoutPage = ({ sendOrderStart, cartItems, total, orderDetails }) => {
 
   return (
     <div>
-
       <CheckoutPageContainer>
         <CheckoutHeaderContainer>
           <HeaderBlockContainer>
@@ -92,151 +102,141 @@ const CheckoutPage = ({ sendOrderStart, cartItems, total, orderDetails }) => {
           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
 
-        {/* <WarningContainer>
-          *Folosește următorul card de credit pentru testare*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </WarningContainer>
-        <StripeCheckoutButton price={total} /> */}
+        {cartItems.length > 0 &&
 
-        <CheckoutItemContainer>
-          <form className='checkout-form' onSubmit={handleSubmit}>
-            <Accordion allowMultipleExpanded={true} allowZeroExpanded={true}>
+          <CheckoutItemContainer>
+            <form className='checkout-form' onSubmit={handleSubmit}>
+              <Accordion preExpanded={['deliveryAddress', 'paymentMethod']} allowMultipleExpanded={true} allowZeroExpanded={true}>
 
-              <AccordionItem>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    Adresa de livrare
+                <AccordionItem uuid='deliveryAddress'>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      Adresa de livrare
                   </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                  <FormInput
-                    type='text'
-                    name='displayName'
-                    value={displayName}
-                    onChange={handleChange}
-                    label='Persoană de contact'
-                    required
-                  />
-                  <FormInput
-                    type='text'
-                    name='phoneNumber'
-                    value={phoneNumber}
-                    onChange={handleChange}
-                    label='Număr de telefon pentru contact'
-                    required
-                  />
-                  <FormInput
-                    type='text'
-                    name='country'
-                    value={'România'}
-                    onChange={handleChange}
-                    label='Țara'
-                    disabled
-                  />
-                  <FormInput
-                    type='text'
-                    name='region'
-                    value={region}
-                    onChange={handleChange}
-                    label='Județ'
-                    required
-                  />
-                  <FormInput
-                    type='text'
-                    name='city'
-                    value={city}
-                    onChange={handleChange}
-                    label='Oraș'
-                    required
-                  />
-                  <FormInput
-                    type='text'
-                    name='completeAddress'
-                    value={completeAddress}
-                    onChange={handleChange}
-                    label='Strada, Număr (*Bloc, Scară, Apartament, Etaj)'
-                    required
-                  />
-                  <FormInput
-                    type='text'
-                    name='otherInstructions'
-                    value={otherInstructions}
-                    onChange={handleChange}
-                    label='Alte instrucțiuni (opțional)'
-                  />
-                </AccordionItemPanel>
-              </AccordionItem>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <FormInput
+                      type='text'
+                      name='displayName'
+                      value={displayName}
+                      onChange={handleChange}
+                      label='Persoană de contact'
+                    />
+                    <FormInput
+                      type='text'
+                      name='phoneNumber'
+                      value={phoneNumber}
+                      onChange={handleChange}
+                      label='Număr de telefon pentru contact'
+                    />
+                    <FormInput
+                      type='text'
+                      name='country'
+                      value={'România'}
+                      onChange={handleChange}
+                      label='Țara'
+                      disabled
+                    />
+                    <FormInput
+                      type='text'
+                      name='region'
+                      value={region}
+                      onChange={handleChange}
+                      label='Județ'
+                    />
+                    <FormInput
+                      type='text'
+                      name='city'
+                      value={city}
+                      onChange={handleChange}
+                      label='Oraș'
+                    />
+                    <FormInput
+                      type='text'
+                      name='completeAddress'
+                      value={completeAddress}
+                      onChange={handleChange}
+                      label='Strada, Număr (*Bloc, Scară, Apartament, Etaj)'
+                    />
+                    <FormInput
+                      type='text'
+                      name='otherInstructions'
+                      value={otherInstructions}
+                      onChange={handleChange}
+                      label='Alte instrucțiuni (opțional)'
+                    />
+                  </AccordionItemPanel>
+                </AccordionItem>
 
-              <AccordionItem>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    Metoda de livrare și plată
+                <AccordionItem uuid='paymentMethod'>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      Metoda de livrare și plată
               </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
 
-                  <div className='group-checkout'>
-                    <div>
-                      <input
-                        type='radio'
-                        id='paymentMethod1'
-                        value='bankTransfer'
-                        checked={paymentMethod === 'bankTransfer'}
-                        onChange={handleRadioChange}
-                      />
-                      <label htmlFor="paymentMethod1">Transfer Bancar - (0 cost transport)</label>
-                    </div>
+                    <div className='group-checkout'>
+                      <div>
+                        <input
+                          type='radio'
+                          id='paymentMethod1'
+                          value='bankTransfer'
+                          checked={paymentMethod === 'bankTransfer'}
+                          onChange={handleRadioChange}
+                        />
+                        <label htmlFor="paymentMethod1">Transfer Bancar - (0 cost transport)</label>
+                      </div>
 
-                    <div>
-                      <input
-                        type='radio'
-                        id='paymentMethod2'
-                        value='ramburs'
-                        checked={paymentMethod === 'ramburs'}
-                        onChange={handleRadioChange}
-                      />
-                      <label htmlFor="paymentMethod2">Poșta română - Ramburs (15 ron transport)</label>
-                    </div>
+                      <div>
+                        <input
+                          type='radio'
+                          id='paymentMethod2'
+                          value='ramburs'
+                          checked={paymentMethod === 'ramburs'}
+                          onChange={handleRadioChange}
+                        />
+                        <label htmlFor="paymentMethod2">Poșta română - Ramburs (15 ron transport)</label>
+                      </div>
 
-                    <div>
-                      <input
-                        type='radio'
-                        id='paymentMethod3'
-                        value='pickup'
-                        checked={paymentMethod === 'pickup'}
-                        onChange={handleRadioChange}
-                        label='Ridicare personala in Medias ( 0 cost transport)'
-                      />
-                      <label htmlFor="paymentMethod3">Ridicare personala in Medias ( 0 cost transport)</label>
+                      <div>
+                        <input
+                          type='radio'
+                          id='paymentMethod3'
+                          value='pickup'
+                          checked={paymentMethod === 'pickup'}
+                          onChange={handleRadioChange}
+                          label='Ridicare personala in Medias ( 0 cost transport)'
+                        />
+                        <label htmlFor="paymentMethod3">Ridicare personala in Medias ( 0 cost transport)</label>
+                      </div>
+                      <div>
+                        <input
+                          type='radio'
+                          id='paymentMethod4'
+                          value='creditCard'
+                          checked={false}
+                          disabled
+                          onChange={handleRadioChange}
+                          label='*(in curand) Plata cu cardul - (0 cost transport)'
+                        />
+                        <label htmlFor="paymentMethod4">*(in curand) Plata cu cardul - (0 cost transport)</label>
+                      </div>
                     </div>
-                    <div>
-                      <input
-                        type='radio'
-                        id='paymentMethod4'
-                        value='creditCard'
-                        checked={false}
-                        disabled
-                        onChange={handleRadioChange}
-                        label='*(in curand) Plata cu cardul - (0 cost transport)'
-                      />
-                      <label htmlFor="paymentMethod4">*(in curand) Plata cu cardul - (0 cost transport)</label>
-                    </div>
-                  </div>
-                </AccordionItemPanel>
-              </AccordionItem>
-            </Accordion>
-            <TotalContainer>Preț total: {totalPriceWithTransport} RON</TotalContainer>
-            <TotalContainer>Error message: {orderDetails}</TotalContainer>
-            <TotalContainer>
-              {/* onClick={() => sendOrderSuccess(orderProperties)} */}
-              <AddButton type='submit' inverted>
-                Trimite comanda
+                  </AccordionItemPanel>
+                </AccordionItem>
+              </Accordion>
+              <TotalContainer>Preț total: {totalPriceWithTransport} RON</TotalContainer>
+              <TotalContainer>Error message: {orderDetails}</TotalContainer>
+              <TotalContainer>
+                {/* onClick={() => sendOrderSuccess(orderProperties)} */}
+                <AddButton type='submit' inverted>
+                  Trimite comanda
             </AddButton>
-            </TotalContainer>
-          </form>
-        </CheckoutItemContainer>
-
+              </TotalContainer>
+            </form>
+          </CheckoutItemContainer>
+        }
 
       </CheckoutPageContainer>
 
