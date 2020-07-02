88e1f00a4c81794533a9787fbfd56b2f58ab955c
Andrea Calin <andrea.n.calin@gmail.com>
2020-07-02 16:56:13 +0200

    deps upgraded and notification for add item

--- client/src/components/item-detail/item-detail.component.jsx
+++ client/src/components/item-detail/item-detail.component.jsx
@@ -7,59 +7,69 @@ import { withRouter } from 'react-router-dom';
 
 import SimpleImageSlider from "react-simple-image-slider";
 
+import { useToasts } from 'react-toast-notifications'
 
-class ItemDetail extends React.Component {
 
-    render() {
-        const { item, title } = this.props.location.state;
-        const img = [];
+const ItemDetail = (state) => {
 
-        let images = [];
-        images = item.additionalImages ? images.concat(item.imageUrl).concat(item.additionalImages) : images.concat(item.imageUrl);
-        images.map(image => img.push({ url: image }))
-        return (
-            <div>
-                <NavigationLinkContainer href={`/shop/`}>Magazin / </NavigationLinkContainer>
-                <NavigationLinkContainer href={`/shop/${title.toLowerCase()}`}>{title}</NavigationLinkContainer>
-                <div className="row">
-                    <h1>
-                        {item.name}
-                    </h1>
-                </div>
-                <div className="row">
-                    <div>
-                        <div>
-                            <SimpleImageSlider
-                                width={350}
-                                height={450}
-                                images={img}
-                                slideDuration={0.3}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div>
-                        <span><b>Preț: {item.price} ron</b></span>
-                    </div>
-                </div>
-                <div className="row">
-                    <div>
-                        <DescriptionContainer>{item.description}</DescriptionContainer>
-                    </div>
-                </div>
-                <div className="row">
-                    <div>
-                        <AddButton onClick={() => this.props.addItem(item)} inverted>
-                            Adaugă în coș
-                        </AddButton>
-                    </div>
-                </div>
-            </div >
+    const { location } = state;
+    const { item, title } = location.state;
+    const img = [];
 
-        );
+    let images = [];
+    images = item.additionalImages ? images.concat(item.imageUrl).concat(item.additionalImages) : images.concat(item.imageUrl);
+    images.map(image => img.push({ url: image }))
+
+    const { addToast } = useToasts()
+
+    const addItemToCart = item => {
+
+        state.addItem(item);
+        addToast("Produsul a fost adăugat în coșul de cumpărături!", { appearance: 'success' })
 
     }
+
+    return (
+        <div>
+            <NavigationLinkContainer href={`/shop/`}>Magazin / </NavigationLinkContainer>
+            <NavigationLinkContainer href={`/shop/${title.toLowerCase()}`}>{title}</NavigationLinkContainer>
+            <div className="row">
+                <h1>
+                    {item.name}
+                </h1>
+            </div>
+            <div className="row">
+                <div>
+                    <div>
+                        <SimpleImageSlider
+                            width={350}
+                            height={450}
+                            images={img}
+                            slideDuration={0.3}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div>
+                    <span><b>Preț: {item.price} ron</b></span>
+                </div>
+            </div>
+            <div className="row">
+                <div>
+                    <DescriptionContainer>{item.description}</DescriptionContainer>
+                </div>
+            </div>
+            <div className="row">
+                <div>
+                    <AddButton onClick={() => addItemToCart(item)} inverted>
+                        Adaugă în coș
+                        </AddButton>
+                </div>
+            </div>
+        </div >
+
+    );
 
 }
 
